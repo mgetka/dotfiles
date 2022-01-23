ba1f60a3934e0e64e7ca62ac2a81a56d576e616c
mgetka <michal.getka@gmail.com>
2022-01-23 12:34:54 +0100

    path ver definition split into topics

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,10 +1,1 @@
-typeset -U PATH_ARRAY
-
-PATH_ARRAY=(
-    "$ZSH/bin"
-    "/opt/homebrew/bin"
-    "/opt/homebrew/opt/coreutils/libexec/gnubin"
-)
-
-export PATH="${(j.:.)PATH_ARRAY}:$PATH"
-unset PATH_ARRAY
+export PATH="$PATH:$ZSH/bin"
