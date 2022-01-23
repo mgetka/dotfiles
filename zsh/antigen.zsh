07338a9c6a12bffc8e16318745da57fb5518fc00
mgetka <michal.getka@gmail.com>
2022-01-23 12:49:41 +0100

    docker & docker-compose auto completions plugins added

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -4,5 +4,7 @@ antigen use oh-my-zsh
 # antigen-bundle zsh-users/zsh-syntax-highlighting
 antigen-bundle zsh-users/zsh-autosuggestions
 antigen-bundle zsh-users/zsh-completions
+antigen-bundle docker
+antigen-bundle docker-compose
 
 antigen-apply
