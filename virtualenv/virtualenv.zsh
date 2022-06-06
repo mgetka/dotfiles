e8c5c090cd2d158b227eb4b92f8e175a4b07c3aa
mgetka <michal.getka@gmail.com>
2022-06-06 08:14:47 +0200

    symlink for rosetta based py3.7 and venv tweaks

--- virtualenv/virtualenv.zsh
+++ virtualenv/virtualenv.zsh
@@ -3,6 +3,3 @@ source /opt/homebrew/bin/virtualenvwrapper.sh
 
 # disables prompt mangling in virtual_env/bin/activate
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-
-# python 3 as default
-export VIRTUALENV_PYTHON=`which python3`
