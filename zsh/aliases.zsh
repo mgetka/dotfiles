c7fd88e318014f1e607439083c5b782f35d1461b
mgetka <michal.getka@gmail.com>
2022-07-21 17:58:26 +0200

    alias on brew in rosetta

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 # by default, run influx cli with hr precision
 alias influx='influx -precision rfc3339'
 
-# create and go to tmp dir 
+# create and go to tmp dir
 alias tmp='cd $(mktemp -d)'
 
 # Get macOS Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
@@ -9,3 +9,6 @@ alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup
 
 # Google Chrome
 alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+
+# Brew... but x86_64
+alias brew86='arch -x86_64 /usr/local/bin/brew'
