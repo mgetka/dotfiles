4317cdc3671ff06e3a5a85cd16c0ec2d1f8fb74c
mgetka <michal.getka@gmail.com>
2024-06-06 15:29:40 +0200

    drop macos specific aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,12 +3,3 @@ alias influx='influx -precision rfc3339'
 
 # create and go to tmp dir
 alias tmp='cd $(mktemp -d)'
-
-# Get macOS Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
-alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g'
-
-# Google Chrome
-alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
-
-# Brew... but x86_64
-alias brew86='arch -x86_64 /usr/local/bin/brew'
