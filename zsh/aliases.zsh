377291ac851b1346382fd85dde5e6bd37579d2b8
mgetka <michal.getka@gmail.com>
2025-11-21 10:04:14 +0100

    new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,6 @@ alias influx='influx -precision rfc3339'
 
 # create and go to tmp dir
 alias tmp='cd $(mktemp -d)'
+
+# absolutely clean chrome session
+alias virgin-chrome='google-chrome --user-data-dir=$(mktemp -d) --disable-fre --no-default-browser-check --no-first-run'
